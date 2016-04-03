d416af4c34b7feedc557a7326cb4fc767fdf3981
LaurieCheers <lauriecheers@gmail.com>
2016-04-03 20:39:56 +0100

    More emails, box for dscrypt

--- NDAarchives.js
+++ NDAarchives.js
@@ -6,8 +6,27 @@ NDAarchives = {
             "subject": "Travel arrangements",
             "text": ["Will you be coming to the party tonight? If so there's a bus that leaves "+
             "Oldtown at 6:00. Hope to see you there?"]
+        },
+        {
+            "sender": "Samuel Beers",
+            "dateTime": "Mon Sep 2nd, 11:00AM",
+            "subject": "Unacceptable Attendance",
+            "text": ["You were late in on Friday and you're not here yet today. We've talked about "+
+            "your attendance before, and frankly I've had enough of discussing it. I respect your "+
+            "work, Bryce, but my tolerance has limits. I don't know "+
+            "what it'll take to sort your life out, but maybe some motivation will help: " +
+            "If you're late again this month, you're out of a job. "]
         }
     ],
     "baycole66": [
+        {
+            "sender": "Bjorn Z Madsen",
+            "dateTime": "Mon Sep 2nd, 11:00AM",
+            "subject": "Looking forward to D-Con",
+            "text": ["Hi, I just wanted to say I'm very much looking forward to your speech at "+
+                "D-Con next week! Hope we can meet up afterwards, I believe I'm working on a "+
+                "project that's right up your alley. (I also have a sample of a new thai tea "+
+                "blend that I believe you will find very refreshing.)"]
+        }
     ],
 };
