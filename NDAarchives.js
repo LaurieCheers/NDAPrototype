3a796bb9f7352de333a7ad3b0a4ddc5a30cda3be
LaurieCheers <lauriecheers@gmail.com>
2016-04-03 10:08:57 +0100

    First version

---
+++ NDAarchives.js
@@ -0,0 +1,13 @@
+NDAarchives = {
+    "bryce81": [
+        {
+            "sender": "Cathy Robson",
+            "dateTime": "Sun Sep 1st, 11:00AM",
+            "subject": "Travel arrangements",
+            "text": ["Will you be coming to the party tonight? If so there's a bus that leaves "+
+            "Oldtown at 6:00. Hope to see you there?"]
+        }
+    ],
+    "baycole66": [
+    ],
+};
